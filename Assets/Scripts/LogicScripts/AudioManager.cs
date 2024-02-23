173d831d47d90dc8711e977f0723107e7168eaf9
Alon Fibert <alonfib@gmail.com>
2024-02-23 17:31:57 +0200

    audio logic inserted but not used

---
+++ Assets/Scripts/LogicScripts/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource failAudioSource; // Assign in inspector
+    public AudioSource winAudioSource; // Assign in inspector
+
+    public void PlayWinSound() {
+        winAudioSource.Play();
+    }
+
+    public void PlayFailSound()
+    {
+        failAudioSource.Play();
+    }
+}
